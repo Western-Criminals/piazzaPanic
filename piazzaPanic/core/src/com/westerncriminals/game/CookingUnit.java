24e58780360c82248f52123c3f1f2d60447bc738
Dipken Fung <23611770+LuciferLordKing@users.noreply.github.com>
2023-01-04 22:52:07 +0000

    Update CookingUnit.java
    pain

--- piazzaPanic/core/src/com/westerncriminals/game/CookingUnit.java
+++ piazzaPanic/core/src/com/westerncriminals/game/CookingUnit.java
@@ -1,1 +1,21 @@
-public class CookingUnit {}
+public class CookingUnit {
+    private String dish;
+    private boolean occupied;
+
+    public void setDish(Dish dish) {
+        this.dish = dish.getDishName();
+        this.occupied = true;
+    }
+
+    public void removeDish() {
+        this.occupied = false;
+    }
+
+    public String getDish() {
+        if (this.occupied == false) {
+            return "This slot is empty.";
+        } else {
+            return this.dish;
+        }
+    }
+}
