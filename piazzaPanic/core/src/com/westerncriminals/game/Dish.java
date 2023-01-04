4e62fdf013fe08abc86d883bb035dcdd58ae5653
Dipken Fung <23611770+LuciferLordKing@users.noreply.github.com>
2023-01-04 22:31:55 +0000

    Create Dish.java
    pain

---
+++ piazzaPanic/core/src/com/westerncriminals/game/Dish.java
@@ -0,0 +1,20 @@
+public Dish {
+    private String name;
+    private int duration;
+
+    public void setDishName(String name) {
+        this.name = name;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getDishName() {
+        return this.name;
+    }
+
+    public String getDuration() {
+        return this.duration;
+    }
+}
