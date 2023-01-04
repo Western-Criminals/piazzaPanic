6c9c4720f2f09c8f8872700085010d5b721c8d07
Dipken Fung <23611770+LuciferLordKing@users.noreply.github.com>
2023-01-04 18:54:07 +0000

    Update Chef.java
    pain

--- piazzaPanic/core/src/com/westerncriminals/game/Chef.java
+++ piazzaPanic/core/src/com/westerncriminals/game/Chef.java
@@ -1,2 +1,12 @@
 public class Chef {
+    private String chefName;
+    private boolean busy;
+
+    public void setState(boolean busy) {
+        this.busy = busy;
+    }
+
+    public boolean getState() {
+        return this.busy;
+    }
 }
