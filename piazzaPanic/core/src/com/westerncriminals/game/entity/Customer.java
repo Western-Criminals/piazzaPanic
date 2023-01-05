7e1112bb6f99ff44d6f2567770a4d1d8b0d01b66
Dipken Fung <23611770+LuciferLordKing@users.noreply.github.com>
2023-01-05 21:16:50 +0000

    Create Customer.java
    pain

---
+++ piazzaPanic/core/src/com/westerncriminals/game/entity/Customer.java
@@ -0,0 +1,10 @@
+public class Customer {
+    private String customerName;
+
+    public void getCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public boolean getCustomerName() {
+        return this.customerName;
+    }
